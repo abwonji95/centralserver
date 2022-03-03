e8c92402d2fb10399e33e4edb58f97442576a3ae
abwonji95 <abwonji95@gmail.com>
2022-03-03 16:24:36 +0300

    fixes

--- csapp/models.py
+++ csapp/models.py
@@ -11,20 +11,25 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-class User(AbstractUser):
+class User(User):
 
-    first_name=models.CharField(max_length=200,null=True,blank=True)
-    last_name=models.CharField(max_length=200,null=True,blank=True)
     email_verified = models.BooleanField(default=False)
     phone = models.CharField(max_length=20,null=True, blank=True, unique=True)
-    username = None
-    email=models.EmailField(max_length=200,null=True,unique=True)
     date_created=models.DateTimeField(auto_now_add=True)
     date_updated=models.DateTimeField(auto_now=True)
   
     def __str__(self) :
         return "{} {}".format(self.first_name,self.last_name)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+
+class Article(models.Model) :
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    date_created=models.DateTimeField(auto_now_add=True)
+    date_updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return "{} {}".format(self.title,self.author)
 
     
